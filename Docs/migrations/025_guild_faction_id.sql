a9e75eb4e9f7d39832859ecac57633ced90eb9cb
CalebKious <calebkious1@gmail.com>
2026-04-02 12:58:46 -0500

    Remove Docs/ from tracking (gitignored)

--- Docs/migrations/025_guild_faction_id.sql
+++
@@ -1,5 +0,0 @@
--- Guild faction binding: guilds belong to a faction, only same-faction members allowed
-ALTER TABLE guilds ADD COLUMN IF NOT EXISTS faction_id SMALLINT NOT NULL DEFAULT 0;
-
--- Index for queries filtering guilds by faction
-CREATE INDEX IF NOT EXISTS idx_guilds_faction_id ON guilds (faction_id) WHERE is_disbanded = false;
